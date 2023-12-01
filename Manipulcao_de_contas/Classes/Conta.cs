26e564805de59ededeb9073b289409282136bbe4
KommissarAndersonVargas <andersomrobertomachadodevargas@gmail.com>
2023-12-01 11:59:28 -0300

    Adicionar arquivos de projeto.

---
+++ Manipulcao_de_contas/Classes/Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+     class Conta
+    {
+    public int ID { get; set; }
+    public string nome { get; set; }
+
+    public double dinheiro { get; set; }
+
+    public Conta(int iD, string nome, double dinheiro)
+    {
+        ID = iD;
+        this.nome = nome;
+        this.dinheiro = dinheiro;
+    }
+
+    public void AddMoney(double valor)
+    {
+        dinheiro = valor + dinheiro;
+    }
+    public void SubtraMoney(double valor)
+    {
+        dinheiro = dinheiro - valor;
+    }
+    public void Transfer(Conta client, double valor)
+    {
+        client.AddMoney(valor);
+        SubtraMoney(valor);
+    }
+
+    public override string ToString()
+    {
+        return "----------------------------"
+                 +
+                "\nNome: " + nome + "\n" + "Id: " + ID + "\n" + "Dinheiro: " + dinheiro +
+                "\n---------------------------\n";
+            
+            
+    }
+}
+
+
---
+++ Manipulcao_de_contas/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Conta person0 = new Conta(23, "Jose Jesus", 5600);
+Conta person1 = new Conta(25, "Pedro Bobbi", 4600);
+
+Console.WriteLine(person0);
+Console.WriteLine(person1);
+person1.SubtraMoney(300); // seubtrai 300
+Console.WriteLine(person1);
+person0.AddMoney(300); // Adiciona 300
+Console.WriteLine(person0);
+person1.Transfer(person0, 300); // transefre 300 para Jose Jesus
+Console.WriteLine(person0);
+Console.WriteLine(person1);
